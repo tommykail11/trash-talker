dd85d13f130c1ff914ad02db3f41279e03551125
Tommy Kail <tommykail@gmail.com>
2013-04-17 21:39:03 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrashTalker::Application.config.secret_token = 'e47086aa6f8badb5f33ae8b93ec93ebb85e3b835b45b6f789c156e744eab8d3418669727979aa614291e6ac305844bb43b6d8abf2cf96e7268770b78fb07d7dd'
